8e4716371e1a3c9297c5bd42463923f7b305b223
Nitya Ranjan <43794015+nityaranjan22@users.noreply.github.com>
2023-03-30 23:25:50 -0400

    Added necessary folders to start coding

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Define your routes and models here
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
